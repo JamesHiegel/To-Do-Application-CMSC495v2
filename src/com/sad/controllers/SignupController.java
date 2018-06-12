c41b948b8761c33d0329326b590a90561a39265c
Swapnil <swapnilp618@gmail.com>
2018-06-12 11:23:19 -0400

    ##UPDATED TEMP
    Added login fxml and controller.

---
+++ src/com/sad/controllers/SignupController.java
@@ -0,0 +1,34 @@
+package com.sad.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignupController implements Initializable {
+
+    @FXML
+    Label signup_banner_snup;
+    @FXML
+    Label uname_label_snup;
+    @FXML
+    TextField uname_snup;
+    @FXML
+    Label pfield_label_snup;
+    @FXML
+    PasswordField pfield_snup;
+    @FXML
+    Button snup_button_snup;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
