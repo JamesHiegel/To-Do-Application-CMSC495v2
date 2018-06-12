e4d30ec17187a63a1236d65a0c4ce39f2a82d18d
Swapnil <swapnilp618@gmail.com>
2018-06-12 12:14:33 -0400

    ##UPDATED TEMP
    put a template how to create an action handler in logincontroller.
    
    let me know if we want to do this differently.

--- src/com/sad/controllers/LoginController.java
+++ src/com/sad/controllers/LoginController.java
@@ -2,25 +2,35 @@ package com.sad.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javafx.event.*;
+import javafx.scene.control.TextField;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController  implements Initializable {
 
 
-    @FXML Label banner_login_login;
-    @FXML Label uname_label_login;
-    @FXML TextField uname_login;
-    @FXML Label pwrd_label_login;
-    @FXML PasswordField pfield_login;
-    @FXML Button login_button_login;
-    @FXML Label noacc_label_login;
-    @FXML Button snup_button_login;
+    @FXML
+    Label banner_login_login;
+    @FXML
+    Label uname_label_login;
+    @FXML
+    TextField uname_login;
+    @FXML
+    Label pwrd_label_login;
+    @FXML
+    PasswordField pfield_login;
+    @FXML
+    Button login_button_login;
+    @FXML
+    Label noacc_label_login;
+    @FXML
+    Button snup_button_login;
 
 
 
@@ -28,15 +38,18 @@ public class LoginController  implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        login_button_login.addActionListener( e -> {
-
-            //add action event
-
-        } );
-
+        //reference the handler
+        login_button_login.setOnAction( this::login_handleButtonAction );
 
 
     }
+
+    private void login_handleButtonAction(ActionEvent event) {
+
+        //what happens after the button is clicked
+
+    }
+
 
 
 
