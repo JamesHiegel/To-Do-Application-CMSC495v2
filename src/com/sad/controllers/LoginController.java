8b153ce908773aa59ce420e5694c584226cda6f2
Swapnil <swapnilp618@gmail.com>
2018-06-12 12:34:20 -0400

    ##UPDATED TEMP
    put a template how to create an action handler in logincontroller.
    
    let me know if we want to do this differently.

--- src/com/sad/controllers/LoginController.java
+++ src/com/sad/controllers/LoginController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.PasswordField;
 import javafx.event.*;
 import javafx.scene.control.TextField;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -38,6 +39,9 @@ public class LoginController  implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        //when this scene is initialized, the only thing we need to ensure as well, is the login button is ready to go!
+
+
         //reference the handler
         login_button_login.setOnAction( this::login_handleButtonAction );
 
@@ -47,6 +51,43 @@ public class LoginController  implements Initializable {
     private void login_handleButtonAction(ActionEvent event) {
 
         //what happens after the button is clicked
+
+        //do we use a string connected to a different class?
+        String login_getUserName = uname_login.getText();
+
+        /**
+         How do we get the password and verify it? this is important and an aspect of security.
+         Everything that must happen after the button is clicked, must be typed here, including scene changes and etc.
+         */
+
+        String login_pfield_retrieve = pfield_login.getText();
+
+
+        //this is where we check where the uname & passwords match or not
+
+        try {
+            /**
+            String buffer_getunamefromdb = dbclass.getunamefromdb (class methods getters)
+             String buffer_getpnamefromdb = dbclass.getpnamefromdb (class methods getters)
+            if(login_getUsername.equals(buffer_getnamefromdb) && login_pfield_retrieve.equals(buffer_getpnamefromdb)  ){
+
+             //Authenticate here and redirect to next scene
+
+            }else{
+
+             //else show wrong password prompt
+
+             //else redirect to unsuccessfull login screen after 3 attempts
+
+
+             }
+            */
+
+        } catch (Exception e) {
+
+            //Exception can be changed accordingly
+            e.printStackTrace();
+        }
 
     }
 
