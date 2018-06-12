9936d2b8d7f82587f5fd1bad447d1f29563ada9e
Swapnil <swapnilp618@gmail.com>
2018-06-12 10:51:41 -0400

    ##Update TEMP
    Added a sign-up button.

--- src/com/sad/controllers/LoginController.java
+++ src/com/sad/controllers/LoginController.java
@@ -11,12 +11,16 @@ import java.util.ResourceBundle;
 public class LoginController  implements Initializable {
 
 
-    @FXML private Label banner_login_login;
-    @FXML private Label uname_label_login;
-    @FXML private TextField uname_login;
-    @FXML private Label pwrd_label_login;
-    @FXML private PasswordField pfield_login;
-    @FXML private Button login_button_login;
+    @FXML Label banner_login_login;
+    @FXML Label uname_label_login;
+    @FXML TextField uname_login;
+    @FXML Label pwrd_label_login;
+    @FXML PasswordField pfield_login;
+    @FXML Button login_button_login;
+    @FXML Label noacc_label_login;
+    @FXML Button snup_button_login;
+
+
 
 
     @Override
