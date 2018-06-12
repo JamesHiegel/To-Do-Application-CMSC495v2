44aed53e129af7fd70d84c78a99825d569a213ec
Swapnil <swapnilp618@gmail.com>
2018-06-12 09:43:43 -0400

    ##TEMPORARY
    Added login FXML elements, will update with final code after meeting

--- src/com/sad/controllers/LoginController.java
+++ src/com/sad/controllers/LoginController.java
@@ -1,15 +1,33 @@
 package com.sad.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController  implements Initializable {
 
+
+    @FXML private Label banner_login_login;
+    @FXML private Label uname_label_login;
+    @FXML private TextField uname_login;
+    @FXML private Label pwrd_label_login;
+    @FXML private PasswordField pfield_login;
+    @FXML private Button login_button_login;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
+
     }
+
+
+
+
 
 }
